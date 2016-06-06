2ca82fa31ab2de6fee0f1e832cdc336e8572458b
jung.m.tim@gmail.com <Guest@epicodus-13.local>
2016-06-06 13:29:44 -0700

    fixed js some more

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,13 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val()
-    var person2Input = $("input#person2").val()
-    var animalInput= $("input#animal").val()
-    var exclamationInput = $("input#exclamation").val()
-    var verbInput = $("input#verb").val()
-    var nounInput = $("input#noun").val()
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
-
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+  $("#list").submit(function(event) {
+    var blanks = ["vegetable", "protein", "beverage", "fruit", "snack", "misc"];
 
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
+      $("#show").append("<li>" + userInput + "</li>");
     });
 
-    $("#story").show();
+
 
     event.preventDefault();
   });
