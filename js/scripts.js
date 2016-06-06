4b07205b3187a521a80951eb3ab9f60fac33e250
jung.m.tim@gmail.com <Guest@epicodus-13.local>
2016-06-06 15:43:55 -0700

    finished grocery w/ help from friend

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,27 @@ $(document).ready(function() {
   $("#list").submit(function(event) {
     var blanks = ["vegetable", "protein", "beverage", "fruit", "snack", "misc"];
 
+    var food = [];
+
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
-      $("#show").append("<li>" + userInput + "</li>");
+      food.push(userInput);
+      $("#list").hide();
     });
+
+    food.sort();
+    food.toUpperCase();
+
+    food.forEach(function(sort){
+    $("#show").append("<li>" + sort + "</li>");
+  });
+
+    // blanks.forEach(function(blank) {
+    //   var userInput = $("input#" + blank).val();
+    //   $("#show").append("<li>" + userInput + "</li>");
+    // });
+
+
 
 
 
